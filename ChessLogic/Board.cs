e6d8a388b882cbf6de702c28e4aaa6a1db24283f
Brandon Schliep <schliepb38@gmail.com>
2023-10-05 18:12:23 -0600

    Add project files.

---
+++ ChessLogic/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Board
+    {
+        private readonly Piece[,] Pieces = new Piece[8, 8];
+        public Piece this[int row, int col]
+        {
+            get { return Pieces[row, col]; }
+            set { Pieces[row, col] = value; }
+        }
+
+        public Piece this[Position pos]
+        {
+            get { return Pieces[pos.Row, pos.Column]; }
+            set { this[pos.Row, pos.Column] = value; }
+        }
+
+        // Left off here
+
+    }
+}
---
+++ ChessLogic/Position.cs
@@ -0,0 +1,53 @@
+﻿namespace ChessLogic
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column) 
+        {
+            Row = row;
+            Column = column;
+        }
+        /// ---------------------------------------------------------
+        ///  Determines the Color of the square on the Grid
+        /// ---------------------------------------------------------
+        public Player SquareColor()
+        {
+            if ((Row + Column) % 2 == 0)
+            {
+                // Returns the color of the square
+                return Player.White;
+            }
+            return Player.Black;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta);
+        }
+    }
+}
